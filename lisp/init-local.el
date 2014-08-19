530b4328dfa8ddfa3c0bb06bf912bdd903fa4de7
qianwan <qianwan@yahoo-inc.com>
2014-08-19 11:02:48 +0800

    Add maxima mode and ibuffer shortcut

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -23,5 +23,17 @@
 
 (require 'cltl2)
 
+;;; maxima
+(add-to-list 'load-path "/usr/local/Cellar/maxima/5.33.0/share/maxima/5.33.0/emacs/")
+(autoload 'maxima-mode "maxima" "Maxima mode" t)
+(autoload 'imaxima "imaxima" "Frontend for maxima with Image support" t)
+(autoload 'maxima "maxima" "Maxima interaction" t)
+(autoload 'imath-mode "imath" "Imath mode for math formula input" t)
+(setq imaxima-use-maxima-mode-flag t)
+(add-to-list 'auto-mode-alist '("\\.ma[cx]" . maxima-mode))
+
+;;; shortcut
+(global-set-key (kbd "<f5>") 'ibuffer)
+
 (provide 'init-local)
 ;;; init-local.el ends here

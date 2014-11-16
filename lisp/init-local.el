7b42499e948b5dcf8bca75ba6b59443016f67016
qianwan <qianwan@yahoo-inc.com>
2014-11-16 15:19:53 +0800

    Remove w3m

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -37,26 +37,6 @@
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
 
-;;; w3m
-(require-package 'w3m)
-(require 'w3m)
-(setq w3m-mode-map (make-sparse-keymap))
-(define-key w3m-mode-map (kbd "RET") 'w3m-view-this-url)
-(define-key w3m-mode-map (kbd "q") 'bury-buffer)
-(define-key w3m-mode-map (kbd "<mouse-1>") 'w3m-maybe-url)
-(define-key w3m-mode-map [f5] 'w3m-reload-this-page)
-(define-key w3m-mode-map (kbd "C-c C-d") 'haskell-w3m-open-haddock)
-(define-key w3m-mode-map (kbd "M-<left>") 'w3m-view-previous-page)
-(define-key w3m-mode-map (kbd "M-<right>") 'w3m-view-next-page)
-(define-key w3m-mode-map (kbd "M-.") 'w3m-haddock-find-tag)
-(defun w3m-maybe-url ()
-  (interactive)
-  (if (or (equal '(w3m-anchor) (get-text-property (point) 'face))
-          (equal '(w3m-arrived-anchor) (get-text-property (point) 'face)))
-    (w3m-view-this-url)))
-(require 'w3m-haddock)
-(add-hook 'w3m-display-hook 'w3m-haddock-display)
-
 ;;; global key bind
 (global-set-key (kbd "C-x a w") 'delete-other-window)
 

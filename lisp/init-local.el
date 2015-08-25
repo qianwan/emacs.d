28ea15e38c4bcb2b3eb7d9f446d9829a361276af
wanqian <wanqian@conew.com>
2015-08-26 01:10:46 +0800

    optional mode for ensime

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -36,6 +36,12 @@
 (require-package 'ensime)
 (require 'ensime)
 (add-hook 'scala-mode-hook 'ensime-scala-mode-hook)
+;; OPTIONAL
+;; there are some great Scala yasnippets, browse through:
+;; https://github.com/AndreaCrotti/yasnippet-snippets/tree/master/scala-mode
+(add-hook 'scala-mode-hook #'yas-minor-mode)
+;; but company-mode / yasnippet conflict. Disable TAB in company-mode with
+(define-key company-active-map [tab] nil)
 
 ;;; global key bind
 (global-set-key (kbd "C-x a w") 'delete-other-window)

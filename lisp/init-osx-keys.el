6bc1bdcf1318f5a43a4e9e9b03b91e571a01d26b
wanqian_metasota <wanqian@metasota.ai>
2018-10-21 19:25:24 +0800

    osx-keys

--- lisp/init-osx-keys.el
+++ lisp/init-osx-keys.el
@@ -4,6 +4,7 @@
 
 (when *is-a-mac*
   (setq mac-command-modifier 'meta)
+  (setq mac-option-modifier 'meta)
   (setq mac-option-modifier 'none)
   ;; Make mouse wheel / trackpad scrolling less jerky
   (setq mouse-wheel-scroll-amount '(1
